e8312f974495fe3b217f97521e552e312bd653ce
devbydenis <hello.denisrahmadi@gmail.com>
2025-05-19 17:04:37 +0700

    add: implement useSearchParam to filter searching article

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react'
 import Card from '../components/Card'
 import Navbar from '../components/Navbar'
+import { useSearchParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 function HomePage() {
   const [data, setData] = React.useState([])
+  const [searchParam, setSearchParam] = useSearchParams()
+  const {register, handleSubmit} = useForm()
+  // const [inputField, setInputField] = React.useState('')
+
+  // console.log(inputField);
 
   useEffect(() => {
     async function fetchData() {
@@ -19,17 +26,49 @@ function HomePage() {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    console.log(searchParam.get('search'));
+  }, [searchParam])
+
+  const showData = () => {
+    return (
+      data && data.map(article => (
+        <li key={article.slug}>
+          <Card {...article} />
+        </li>
+      ))
+    )
+  }
+
   return (
     <>
       <Navbar />
       <main className='flex flex-col items-center '>
+        <form onSubmit={handleSubmit((data) => setSearchParam({search: data.search}))}>
+          <input className='border border-gray-300 rounded p-2' type="text" placeholder='Search' {...register('search')} />
+          <button type='submit' className='hidden'>Search</button>
+        </form>
+        {
+          searchParam.get("search") && <p>Hasil Pencarian dari <span className='font-bold'>{searchParam.get("search")}</span></p>
+        }
         <ul className=''>
         {
-          data && data.map(article => (
-            <li key={article.slug}>
-              <Card {...article} />
-            </li>
-          ))
+          searchParam.get("search") 
+            ? data
+                .filter(article => article.title.toLowerCase().includes(searchParam.get("search").toLowerCase()))
+                .map(article => (
+                  <li key={article.slug}>
+                    <Card {...article} />
+                  </li>
+                )) 
+            : showData()
+        }
+        {
+          // data && data.map(article => (
+          //   <li key={article.slug}>
+          //     <Card {...article} />
+          //   </li>
+          // ))          
         }
         </ul>
       </main>
